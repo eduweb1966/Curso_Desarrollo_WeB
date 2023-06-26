00e9dc060c1ba29633fc34955e3690f81d3cfc50
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-06-26 13:30:57 +0200

    nuevo file

--- fundacion_once2/JavaScript/03.operadoreslogicos.js
+++
@@ -1,9 +0,0 @@
-//&& -> Y
-// || -> O
-// > -> MAYOR QUE
-// < -> MENOR QUE
-// >= -> MAYOR O IGUAL
-// <= -> MENOR O IGUAL
-// ! -> NEGACION
-
-let boolean = !true;
--- fundacion_once2/README.md
+++
@@ -1,2 +0,0 @@
-# fundacion_once
-En este repositorio se subirán los ejemplos de código vistos en clase
