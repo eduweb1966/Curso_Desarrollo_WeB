9507dc2c36878fb339807f0efb4f6f345da6ba3f
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-07 14:31:49 +0200

    commit final

---
+++ JavaScript/Ejercicios DOM/02.Ejercicio/main.js
@@ -0,0 +1,12 @@
+const div = document.createElement("div");
+div.innerHTML = "Soy el div";
+console.log(div);
+document.body.appendChild(div);
+
+const div = document.createElement("div");
+div.innerHTML = "Soy el div";
+document.body.appendChild(Mydiv)
+for(let i =0; i<6, i ++) {
+const p=document.createElement("p");
+MediaQueryListEvent.appendChild(p)
+}
