9a3449b6d1e50c93f54872bd2b2dd746a92bec8b
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-07 13:52:37 +0200

    commit 21.Ejercicios DOM

--- JavaScriptDOM/09.darkmode/main.js
+++ JavaScriptDOM/09.darkmode/main.js
@@ -1,22 +1,27 @@
-//recuperamos el boton
-const themeBtn = document.querySelector("#theme-btn")
-themeBtn.addEventListener("click", ()=>{
-    // cada vez que hacemos click altrnamos la clase dark, si no tiene el body
-    
-document.body.classList.toggle("dark");
-if(document.body.classList.contains("dark")){
-    themeBtn.id ="🌞";
+//Recuperamos el botón
+const themeBtn = document.querySelector("#theme-btn");
 
-}else{
-    themeBtn.id ="🌑";   
-}
+themeBtn.addEventListener("click", () => {
+  //Cada vez que hacemos click alternamos la clase dark, si no la tiene el body la pone y si la tiene la quita
+  document.body.classList.toggle("dark");
+  //Y además, comprobaremos que si contiene la clase dark que sustituya el texto por un sol y si no por una luna
+  if (document.body.classList.contains("dark")) {
+    themeBtn.innerHTML = "🌞";
+  } else {
+    themeBtn.innerHTML = "🌑";
+  }
 });
-// recuperamos el boton de more
-const main = document.querySelector("main")
-//recuperamos el boton more
-const more-btn = document.querySelector("#more-btn")
-// cuandfo hagamos cvlick en el bot on more...
-themeBtn.addEventListener("click", ()=>{
 
-const para = document.querySelector("main")
- })
+//Recuperamos el main
+const main = document.querySelector("main");
+//Recuperamos el botón de more
+const moreBtn = document.querySelector("#more-btn");
+//Cuando hagamos click en el botón more...
+moreBtn.addEventListener("click"), () => {
+  //Creo un parrafo
+  const paragraph = document.createElement("p");
+  //Le damos contenido al parrafo
+  paragraph.innerHTML = "Hola, soy un nuevo parrafo";
+  //Lo inyectamos en el main
+  main.appendChild(paragraph);
+});
